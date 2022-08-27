e4a247c488fcfcfddb6f1de3b2cf3c63b97fd86b
Dade Lamkins <dade@lamkins.org>
2022-08-27 14:44:42 -0400

    Revised build ID output.

--- general/GetBuildInfo.cs
+++ general/GetBuildInfo.cs
@@ -1,4 +1,30 @@
-﻿var standardCdn = (await "http://assetcdn.101.arenanetworks.com/latest/101".GetStringAsync()).Split(' ');
-var chineseCdn = (await "http://assetcdn.111.cgw2.com/latest/111".GetStringAsync()).Split(' ');
+﻿Func<string, object> parseCdnInfo = (rawCdnResponse) => {
+    string[] cdnVars = rawCdnResponse.Split(' ');
+
+    if (cdnVars.Length == 5) {
+        bool parsedSuccessfully = true;
+
+        parsedSuccessfully &= int.TryParse(cdnVars[0], out int oBuildId);
+        parsedSuccessfully &= int.TryParse(cdnVars[1], out int oExeFileId);
+        parsedSuccessfully &= int.TryParse(cdnVars[2], out int oExeFileSize);
+        parsedSuccessfully &= int.TryParse(cdnVars[3], out int oManifestFileId);
+        parsedSuccessfully &= int.TryParse(cdnVars[4], out int oManifestFileSize);
+
+        if (parsedSuccessfully) {
+            return new { 
+                buildId          = oBuildId,
+                exeFileId        = oExeFileId,
+                exeFileSize      = oExeFileSize,
+                manifestFileId   = oManifestFileId,
+                manifestFileSize = oManifestFileSize
+            };
+        }
+    } else {
+        await SetResponse("FAILED", 500);
+    }
+};
+
+var standardCdn = parseCdnInfo(await "http://assetcdn.101.arenanetworks.com/latest/101".GetStringAsync());
+var chineseCdn  = parseCdnInfo(await "http://assetcdn.111.cgw2.com/latest/111".GetStringAsync());
 
 await SetJsonResponse(new { standard = standardCdn, chinese = chineseCdn }, cacheDuration: TimeSpan.FromMinutes(10));
