4f175642de333fe90f5885c8b34c6fdedbea2864
Xander Lamkins <dade@lamkins.org>
2023-08-24 14:58:18 -0400

    Update GetActiveFestivals.cs

--- general/GetActiveFestivals.cs
+++ general/GetActiveFestivals.cs
@@ -9,17 +9,8 @@
 
 var festivals = new List<string>();
 
-var response = await "https://api.guildwars2.com/v2/achievements/groups/18DB115A-8637-4290-A636-821362A3C4A8".GetJsonAsync();
-
-foreach (int id in response.categories) {
-    if (festivalLookup.ContainsKey(id)) {
-        festivals.Add(festivalLookup[id]);
-    }
-}
-
 if (festivals.Count == festivalLookup.Count) {
     festivals = new List<string>() {
-        "festivalofthefourwinds"
     };
 }
         
